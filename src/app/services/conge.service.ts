a19da9805b1b8d068e2c7b2358b333478e2accdb
mohamedjasserbensmida <74205089+mohamedjasserbensmida@users.noreply.github.com>
2023-09-27 07:15:18 -0700

    final push

--- src/app/services/conge.service.ts
+++ src/app/services/conge.service.ts
@@ -16,15 +16,15 @@ export class CongeService {
     return this.http.get<Conge[]>("http://localhost:8075/conges/getprincipalconge/"+id);
   }
 
-  addNewConge(reclamation:Conge , id:number):Observable<string>{
-    return this.http.post<string>("http://localhost:8075/conges/addNewConge/"+id,reclamation,{responseType: 'text' as 'json'})
+  addNewConge(conge:Conge , id:number):Observable<string>{
+    return this.http.post<string>("http://localhost:8075/conges/addNewConge/"+id,conge,{responseType: 'text' as 'json'})
   }
 
   deleteConge(id:number):Observable<string>{
     return this.http.delete<string>("http://localhost:8075/conges/deleteConge/"+id,{responseType: 'text' as 'json'})
   }
-  updateConge(reclamation:Conge):Observable<string>{
-    return this.http.put<string>("http://localhost:8075/conges/updateConge",reclamation,{responseType:'text' as 'json'})
+  updateConge(conge:Conge):Observable<string>{
+    return this.http.put<string>("http://localhost:8075/conges/updateConge",conge,{responseType:'text' as 'json'})
   }
 
   getAllConge():Observable<Conge[]>{
